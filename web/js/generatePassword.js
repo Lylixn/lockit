4f9410d8600e62113a09226daac2620fd7b0db9f
lylixn <lylian.gr@protonmail.com>
2022-12-09 19:01:14 +0100

    correct generation error

--- web/js/generatePassword.js
+++ web/js/generatePassword.js
@@ -7,22 +7,19 @@ function generatePassword() {
     let length = document.getElementById("length");
 
     for (let i = 0; i <= length.value - 1; i++) {
-        // let char = Math.floor(Math.random() * getValableChar().length);
         let typeChar = getValableChar();
-        switch (typeChar[crypto.randomInt(1, typeChar.length)]) {
+        let aleatoryType = typeChar[crypto.randomInt(0, typeChar.length)];
+        switch (aleatoryType) {
             case "number":
                 let pos1 = crypto.randomInt(1, Character.number.length - 1);
-                console.log(pos1);
                 passwordText += Character.number.charAt(pos1);
                 break;
             case "letter":
                 let pos2 = crypto.randomInt(1, Character.letter.length - 1);
-                console.log(pos2);
                 passwordText += Character.letter.charAt(pos2);
                 break;
             case "special":
                 let pos3 = crypto.randomInt(1, Character.special.length - 1);
-                console.log(pos3);
                 passwordText += Character.special.charAt(pos3);
                 break;
         }
